cc634369421b4a153e036d1e44fcd18da0f0b2eb
unknown <marwahnabilaa@gmail.com>
2026-04-23 21:26:35 +0700

    feat: initial success login automation sauce demo

---
+++ coba.js
@@ -0,0 +1,33 @@
+const chrome = require('selenium-webdriver/chrome')
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+let options = new chrome.Options();
+describe('SauceDemo automation test', function () {
+    let driver;
+
+    it('logs in with standard_user using XPath locators', async function () {
+        driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(options)
+            .build();
+        try { //login
+            await driver.get('https://www.saucedemo.com');
+            await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            await driver.findElement(By.id("login-button")).click();
+        //low to high (acak dulu)
+            await driver.findElement(By.xpath("//select[@data-test='product-sort-container']"));
+            await driver.findElement(By.xpath("//option[text()='Price (low to high)']")).click();
+            await driver.sleep(1500); //jeda
+        // sorting a-z
+            await driver.findElement(By.xpath("//select[@data-test='product-sort-container']")).click();
+            await driver.findElement(By.xpath("//option[@value='az']")).click();
+            await driver.sleep(1500);
+
+        } catch (error) {
+            console.log("Ada error:", error);
+        } finally {
+            await driver.quit();
+        }
+    });
+});
