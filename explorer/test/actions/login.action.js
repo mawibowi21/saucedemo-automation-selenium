bb262bef5897f0330da3091c9f5e09a6f79809b8
unknown <marwahnabilaa@gmail.com>
2026-04-28 11:54:24 +0700

    update project with POM and visual regression

---
+++ explorer/test/actions/login.action.js
@@ -0,0 +1,48 @@
+const LoginPage = require('../pageobjects/login.page');
+const assert = require('assert');
+const { until } = require('selenium-webdriver');
+
+class LoginAction {
+    constructor(driver) {
+        this.driver = driver;
+        this.loginPage = new LoginPage(this.driver); //object page
+    }
+    async openUrl(url) {
+    await this.driver.get(url);
+    }
+    async inputUsername(username) {
+        await this.driver //panggil
+            .findElement(LoginPage.usernameInput)
+            .sendKeys(username);
+    }
+
+    async inputPassword(password) {
+        await this.driver
+            .findElement(LoginPage.passwordInput)
+            .sendKeys(password);
+    }
+
+    async clickLoginButton() {
+        await this.driver
+            .findElement(LoginPage.loginButton)
+            .click();
+    }
+
+    async assertLoginSuccess(header) {
+        await this.driver.wait(until.elementLocated(LoginPage.pageTitle), 5000);
+
+        const title = await this.driver
+            .findElement(LoginPage.pageTitle)
+            .getText();
+
+        assert.strictEqual  (title, header);
+    }
+
+    async assertLoginFailed(expectedErrorMessage) {
+        await this.driver.wait(until.elementLocated(LoginPage.errorMessage), 5000);
+        const actualErrorMessage = await this.driver.findElement(LoginPage.errorMessage).getText();
+        assert.strictEqual(actualErrorMessage, expectedErrorMessage);
+    }
+}
+
+module.exports = LoginAction;
