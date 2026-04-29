2c91639c46d44a109694d086b68defdaabb0963c
unknown <marwahnabilaa@gmail.com>
2026-04-29 13:40:39 +0700

    update: visual regression tests

--- explorer/test/actions/sharing.action.js
+++ explorer/test/actions/sharing.action.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 
 class SharingAction {
     constructor(driver) {
@@ -7,9 +9,11 @@ class SharingAction {
 
     async fullPageScreenshot(fileName) {
         const screenshot = await this.driver.takeScreenshot();
-        fs.writeFileSync('screenshot/' + fileName + '.png', screenshot, 'base64');
+        const actualPath = `screenshot/${fileName}.png`;
+        const expectedPath = `screenshot/${fileName}_expected.png`;
+        const diffPath = `screenshot/${fileName}_diff.png`;
+        fs.writeFileSync(actualPath, screenshot, 'base64');
     }
-
     async partialScreenshot(element, fileName) {
         const partial = await this.driver.findElement(element).takeScreenshot();
         fs.writeFileSync('screenshot/' + fileName + '.png', partial, 'base64');
---
+++ explorer/test/specs/login.visual.test.js
@@ -0,0 +1,59 @@
+const { Builder } = require('selenium-webdriver');
+const LoginAction = require('../actions/login.action');
+const { compareScreenshot }  = require('../utilities/visual_regression.helper')
+
+const SharingAction = require('../actions/sharing.action');
+const Loginpage = require('../pageobjects/login.page');
+describe('Login', () => {
+    let driver; 
+    let loginAction;
+    let sharingAction;
+
+    beforeEach(async () => {
+        driver = new Builder()
+            .forBrowser('chrome')
+            .build();
+
+        loginAction = new LoginAction(driver);
+        sharingAction = new SharingAction(driver);
+        await loginAction.openUrl('https://www.saucedemo.com/');
+    })
+
+    afterEach(async () => {
+        await driver.quit();
+    })
+    //positive
+    it('Login with valid credential', async () => {
+        await loginAction.inputUsername('standard_user');
+        await loginAction.inputPassword('secret_sauce');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginSuccess('Products');
+
+        await compareScreenshot(driver, 'product_page');
+    });
+    //uname salah (kosong) password benar
+    it('Login with empty username', async () => {
+        await loginAction.inputPassword('secret_sauce');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Username is required');
+
+        await compareScreenshot(driver, 'negative_empty_username');
+    });
+    //uname benar password salah (kosong)
+    it('Login with empty password', async () => {
+        await loginAction.inputUsername('standard_user');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Password is required');
+
+        await compareScreenshot(driver, 'negative_empty_password');
+    });
+    //login pakai locked out user
+    it('Login with empty locked out user', async () => {
+        await loginAction.inputUsername('locked_out_user');
+        await loginAction.inputPassword('secret_sauce');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Sorry, this user has been locked out.');
+        
+        await sharingAction.fullPageScreenshot('login_locked_out_user');
+    });
+})
